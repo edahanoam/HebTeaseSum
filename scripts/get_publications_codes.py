bf9b83ffae256e6b3b1449ace340ef5b53f00b87
Noam Dahan <noam.dahan1@cs.huji.ac.il>
2025-09-21 18:12:25 +0300

    publication code

---
+++ scripts/get_publications_codes.py
@@ -0,0 +1,51 @@
+""" Usage:
+    <file-name> [--in=INPUT_FILE] [--out=OUTPUT_FILE] [--debug]
+
+Options:
+  --help                           Show this message and exit
+  -i INPUT_FILE --in=INPUT_FILE    Input file
+                                   [default: infile.tmp]
+  -o INPUT_FILE --out=OUTPUT_FILE  Input file
+                                   [default: outfile.tmp]
+  --debug                          Whether to debug
+"""
+# External imports
+import logging
+import pdb
+from pprint import pprint
+from pprint import pformat
+from docopt import docopt
+from pathlib import Path
+from tqdm import tqdm
+import numpy as np
+import json
+
+# Local imports
+from fetch import fetch_title_codes
+
+# ----
+
+
+if __name__ == "__main__":
+
+    # Parse command line arguments
+    args = docopt(__doc__)
+    inp_fn = args["--in"]
+    out_fn = Path(args["--out"])
+
+    # Determine logging level
+    debug = args["--debug"]
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    logging.info(f"Input file: {inp_fn}, Output file: {out_fn}.")
+
+    # Start computation
+    pub_codes = fetch_title_codes(inp_fn)
+    with open(out_fn, "w", encoding="utf8") as fout:
+        fout.write("\n".join(pub_codes))
+
+    # End
+    logging.info("DONE")
