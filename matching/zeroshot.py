9fb5db32b0c35352f1f11146398e50853fe219bd
Noam Dahan <46026111+edahanoam@users.noreply.github.com>
2025-11-17 16:03:58 +0200

    clean

--- matching/zeroshot.py
+++ matching/zeroshot.py
@@ -13,7 +13,6 @@ from pathlib import Path
 import os
 
 PROMPT="Given the following text and summary, answer with 'Yes' if the text relates to the summary, and 'No' if it does not. Do not provide explanations. Only output 'Yes' or 'No'."
-# model="deepseek-ai/DeepSeek-R1-Distill-Llama-70B-free"
 model="meta-llama/Llama-3.3-70B-Instruct-Turbo-Free"
 
 def wrap_text(text_file):
@@ -26,7 +25,6 @@ def wrap_text(text_file):
 
 def wrap_text_from_df(df,summary_col_name='summary',text_candidate_name= 'text_candidate',cut_text = True):
   # Create a new column 'formatted_output' with the desired format
-  # df.columns = ['summary', 'text_candidate', 'classification']
   if cut_text:
     df[text_candidate_name] = df[text_candidate_name].apply(lambda x: first_n_words(x,500))
 
@@ -99,38 +97,6 @@ def infer_togetherAI_save_results(df,inferences_so_far,outfile="llama_test.csv")
     df.to_csv(outfile, index=False)
 
 
-def infer_togetherAI(df,outfile="llama_test.csv"):
-
-    client = Together()
-    #taking first 3 rows for quic inference, remove later
-    # df = df.head(1)
-    # print(df)
-    inferences = []
-    for i, row in tqdm(df.iterrows(), total=len(df)):
-        cur=False #we didn't get inference for this example
-        while not cur:
-            try:
-                current_prompt = [
-                    {"role": "system", "content": PROMPT},
-                    {"role": "user", "content": row['formatted_output']}
-                    ]
-                response = client.chat.completions.create(
-                model=model,
-                messages=current_prompt,
-                )
-                #inferences.append(response.choices[0].message.content)
-                inferences.append(response.choices[0].message.content)
-                cur=True
-            except:
-                print("Waiting for an hour...")
-                time.sleep(3600)
-                print("continue...")
-
-
-
-    df['inferences'] = inferences
-    df.to_csv(outfile, index=False)
-
 
 def run_on_test_files():
     path_for_texts= 'hadashot_only_relevant.jsonl'
@@ -161,22 +127,15 @@ def run_on_all_data(input_file_candidates,inferences_so_far,outfile,cut_text = T
     df = wrap_text_from_df(df,'main_text','related_text',cut_text)
     print(df.shape)
 
-    #infer_togetherAI(df,outfile)
     infer_togetherAI_save_results(df,inferences_so_far,outfile)
 
 
 
 if __name__=="__main__":
-    # run_on_test_files()
     arguments = docopt(__doc__, version='Filter JSONL 1.0')
-    #this is the the csv file containing all the main texts and the candidate texts and their ids
     input_file_candidates = Path(arguments['--all'])
     outfile = Path(arguments['--out'])
     inferences_so_far = Path(arguments['--inf'])
-    #run_on_all_data(input_file_candidates,inferences_so_far,outfile)
 
-    # df =pd.read_csv(input_file_candidates)
-    # print(df.head(10))
 
-    #for now:
     run_on_all_data(input_file_candidates,inferences_so_far,outfile,True)
